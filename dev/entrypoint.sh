d58d2cdd47f77db579b794a22f3e3d7345b3c2c7
Nikos Koukis <nickkouk@gmail.com>
2020-11-24 11:00:17 +0000

    Add dev Dockerfile and misc files

---
+++ dev/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+function announce() {
+  echo "================================================================================"
+  echo $@
+  echo "================================================================================"
+}
+
+# setup ros environment
+source "$HOME/.bashrc.local"
+
+# Compile the ROS workspace ----------------------------------------------------
+announce "Compiling Workspace packages..."
+compile_ws
+
+/bin/bash
+
